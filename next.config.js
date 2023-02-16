953ceacef30552d642bfe53b6b2a7869b796cdc8
fattyShau <cvcvbnbn13@gmail.com>
2023-02-16 11:09:24 +0800

    [fix]Add Image domain config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['http://p16-sign-va.tiktokcdn.com'],
+    domains: [
+      'http://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/f5f9c186305e769c7ae49bd7ed601aa4%7ec5_720x720.jpeg?',
+    ],
   },
 }
 
